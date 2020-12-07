a57936521bb7c748d63617567bae461c3a2db14e
damr <damr18@gmail.com>
2020-12-07 05:15:44 -0600

    class based components

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className='navbar bg-primary'>
+                <h1>
+                <FontAwesomeIcon icon={faGithub}/> {this.props.title}
+                </h1>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
